f5f2e54255503549d93e412e98f073a699f2dc40
edaro27 <edaro27@gmail.com>
2023-09-06 23:01:04 -0500

    Create window class with a blank canvas using Tkinter library

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # mazesolver
-Boot.dev maze solver project
+Boot.dev mMemphis, Tennesseeaze solver project
 
 We use Tkinter, a Python 3 library, to open a graphical window with a blank canvas that the program will draw on.
+
+Run python -m tkinter from command line to open a window demonstrating the interface.
+
---
+++ main.py
@@ -0,0 +1,37 @@
+from tkinter import Tk, BOTH, Canvas
+import time
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.title = ("Maze solver")
+        self.canvas = Canvas(master = self.__root, width = self.width, height = self.height, bg = "white")
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+
+def main():
+    win = Window(800,600)
+    print("running")
+    win.wait_for_close()
+    time.sleep(10)
+    win.close()
+
+
+
+main()
